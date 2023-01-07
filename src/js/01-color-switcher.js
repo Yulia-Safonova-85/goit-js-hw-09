76cd4118da37141633724712023287826df2f985
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-01-07 23:51:47 +0200

    color-swircher 01_JS

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const color = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+let colorChangeId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+
+function changeBgColor() {
+    let bodyColor = getRandomHexColor(color);
+    document.body.style.background = bodyColor;
+}
+
+function onStartBtnClick() {
+    colorChangeId = setTimeout(() => {
+        changeBgColor(color);
+    }, 1000);
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+}
+
+
+function onStopBtnClick() {
+    clearTimeout(colorChangeId);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
+}
