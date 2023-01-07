ecc3ef6bfc0841976181a688f7de2aaf8a983eb1
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-01-07 21:52:20 +0000

    Deploying to gh-pages from @ Yulia-Safonova-85/goit-js-hw-09@76cd4118da37141633724712023287826df2f985 🚀

---
+++ 01-color-switcher.fe4cbd0a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=null;t.addEventListener("click",(function(){o=setTimeout((function(){var t;t="#".concat(Math.floor(16777215*Math.random()).toString(16)),document.body.style.background=t}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearTimeout(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.fe4cbd0a.js.map
