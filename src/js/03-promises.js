d56b0408f944c43494d211aa89503a4cb5678841
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-01-10 18:58:37 +0200

    add 03_JS

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit() {
+  let delay = Number(form.delay.value);
+
+  for (let i = 1; i <= form.amount.value; i += 1){
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success (`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+    .catch(({ position, delay }) => {
+        Notiflix.Notify.failure (`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(form.step.value);
   }
 }
+
+function createPromise(position, delay) {
+  const object = { position, delay }
+  const promise = new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        res(object)
+      } else {
+        // Reject
+        rej(object)
+      }
+    }, delay)
+  })
+  return promise;
+  }
+
+
+
+  
+
